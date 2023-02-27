be5e292dd7cab905fb4ce0ee6b32c9bcdac1c022
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-27 11:45:36 +0530

    added: Master branch greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day15HashTables
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Master Branch for Day 15 Hast Tables Assignment");
+        }
+    }
+}
