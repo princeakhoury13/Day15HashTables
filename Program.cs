dbe4be1ecf9e555f0a35f358bf8252cf69748f21
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-27 11:55:52 +0530

    added: Findind frequency  of words in given line

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Master Branch for Day 15 Hast Tables Assignment");
+            WordFrequency.Frequency();
         }
     }
 }
